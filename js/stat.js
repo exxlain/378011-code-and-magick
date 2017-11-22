21d0ecd90fe6b1b8f1c634de586e725d0074cdfa
Lana Kostina <exxlain@gmail.com>
2017-11-22 22:30:23 +0700

    Создание функций

--- js/stat.js
+++ js/stat.js
@@ -1,75 +1,84 @@
-window.renderStatistics = function(ctx, names, times) {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-    ctx.beginPath();
-    ctx.moveTo(110, 20);
-    ctx.lineTo(530, 20);
-    ctx.lineTo(610, 140);
-    ctx.lineTo(530, 290);
-    ctx.lineTo(110, 290);
-    ctx.lineTo(30, 140);
-    ctx.lineTo(110, 20);
-    ctx.closePath();
-    ctx.fill();
-
-    ctx.fillStyle = 'white';
-    ctx.beginPath();
-    ctx.moveTo(100, 10);
-    ctx.lineTo(520, 10);
-    ctx.lineTo(600, 130);
-    ctx.lineTo(520, 280);
-    ctx.lineTo(100, 280);
-    ctx.lineTo(20, 130);
-    ctx.lineTo(100, 10);
-    ctx.closePath();
-    ctx.fill();
-
-    ctx.fillStyle = '#000';
-    ctx.font = '16px PT Mono';
-    ctx.fillText('Ура вы победили!', 120, 40);
-    ctx.fillText('Список результатов:', 120, 60);
-
+var findMaxTime = function(arr) {
     var max = -1;
-    var findMaxTime = function(i) {
-        times[i] = Math.floor(times[i]);
-        var time = times[i];
+    for (var i = 0; i < arr.length; i++) {
+        var time = Math.floor(arr[i]);
         if (time > max) {
             max = time;
         }
-        return max;
-    };
-
-    for (var i = 0; i < times.length; i++) {
-        findMaxTime(i);
     }
+    return max;
+};
 
-    var generateColor = function(i) {
-        if (names[i] != 'Вы') {
-            return ctx.fillStyle = 'rgba(0, 0, 255,' + (Math.random() * 0.9 + 0.1) + ')';
-        }
-        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    };
+var getColor = function(isYou) {
+    if (isYou) {
+        return 'rgba(255, 0, 0, 1)';
+    } else {
+        return 'rgba(0, 50, 255, ' + Math.random() + ')';
+    }
+};
 
+var generateText = function(ctx, text, color, coordinateX, coordinateY) {
+    ctx.fillStyle = color;
+    ctx.font = '16px PT Mono';
+    ctx.fillText(text, coordinateX, coordinateY);
+};
+var generateBar = function(ctx, color, coordinateX, coordinateY, barWidth, barHeight) {
+    ctx.fillStyle = color;
+    ctx.fillRect(coordinateX, coordinateY, barWidth, barHeight);
+};
+
+    var widthCloud = 420;
+    var edgeCloudX = 80;
+    var heightCloud = 240;
+    var offset = 10;
+var getCloud = function(ctx, color, coordinateX, coordinateY) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(coordinateX, coordinateY);
+    ctx.lineTo(coordinateX + widthCloud, coordinateY);
+    ctx.lineTo(coordinateX + widthCloud + edgeCloudX, coordinateY + heightCloud / 2);
+    ctx.lineTo(coordinateX + widthCloud, coordinateY + heightCloud);
+    ctx.lineTo(coordinateX, coordinateY + heightCloud);
+    ctx.lineTo(coordinateX - edgeCloudX, coordinateY + heightCloud / 2);
+    ctx.lineTo(coordinateX, coordinateY);
+    ctx.closePath();
+    ctx.fill();
+};
+var getShadow = function(ctx, color, coordinateX, coordinateY) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(coordinateX + offset, coordinateY + offset);
+    ctx.lineTo(coordinateX + widthCloud + offset, coordinateY + offset);
+    ctx.lineTo(coordinateX + widthCloud + edgeCloudX + offset, coordinateY + heightCloud / 2 + offset);
+    ctx.lineTo(coordinateX + widthCloud + offset, coordinateY + heightCloud + offset);
+    ctx.lineTo(coordinateX + offset, coordinateY + heightCloud + offset);
+    ctx.lineTo(coordinateX - edgeCloudX + offset, coordinateY + heightCloud / 2 + offset);
+    ctx.lineTo(coordinateX + offset, coordinateY + offset);
+    ctx.closePath();
+    ctx.fill();
+};
+window.renderStatistics = function(ctx, names, times) {
+    getShadow(ctx, 'black', 100, 10);
+    getCloud(ctx, 'white', 100, 10);
+    generateText(ctx, 'Ура вы победили!', '#000000', 120, 40);
+    generateText(ctx, 'Список результатов:', '#000000', 120, 60);
+
+    findMaxTime(times);
+
+    var indent = 50;
+    var initialX = 150;
+    var initialY = 230;
+    var lineHeight = 15;
     var histogramHeight = 150;
     var step = histogramHeight / (max - 0);
-    var initialX = 140;
-    var initialY = 250;
-    var indent = 90;
-    var barWidth = 40;
-    var lineHeightNames = 20;
-    var lineHeightTimes = 10;
-    var generateBar = function(i) {
-        ctx.fillRect(initialX + indent * i, initialY - times[i] * step, barWidth, times[i] * step);
-    };
-
-    var generateText = function(i) {
-        ctx.fillText(names[i], initialX + indent * i, initialY + lineHeightNames);
-        ctx.fillText(times[i], initialX + indent * i, initialY - times[i] * step - lineHeightTimes);
-    };
 
     for (var i = 0; i < times.length; i++) {
-        generateColor(i);
-        generateBar(i);
-        generateText(i);
-    }
+        var color = getColor(names[i] === 'Вы');
 
+        generateText(ctx, names[i], color, initialX + indent * i, initialY + lineHeight);
+
+        generateText(ctx, times[i], color, initialX + indent * i, initialY - times[i] * step - lineHeight);
+
+        generateBar(ctx, color, initialX + indent * i, initialY - times[i] * step, barWidth, times[i] * step);
+    }
 };
