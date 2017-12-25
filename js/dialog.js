16ce1ed240314a79864f89819c4faf9376c6f6da
Lana Kostina <exxlain@gmail.com>
2017-12-25 17:25:18 +0700

    выкладываем фото

--- js/dialog.js
+++ js/dialog.js
@@ -1,129 +1,144 @@
 'use strict';
-(function() {
-    var setup = document.querySelector('.setup');
-    var setupOpen = document.querySelector('.setup-open');
-    var setupClose = setup.querySelector('.setup-close');
-    var inputName = setup.querySelector('.setup-user-name');
-    var form = setup.querySelector('.setup-wizard-form');
-    /*открытие и закрытие окна*/
+(function () {
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = setup.querySelector('.setup-close');
+  var inputName = setup.querySelector('.setup-user-name');
+  var form = setup.querySelector('.setup-wizard-form');
+  /* открытие и закрытие окна*/
 
-    var openPopup = function () {
-        setup.classList.remove('hidden');
-        document.addEventListener('keydown', onPopupEscPress);
-        setup.style.top = null;
-        setup.style.left = null;
-    };
+  var openPopup = function () {
+    setup.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+    setup.style.top = null;
+    setup.style.left = null;
+  };
 
-    var closePopup = function () {
-        setup.classList.add('hidden');
-        document.removeEventListener('keydown', onPopupEscPress);
-    };
+  var closePopup = function () {
+    setup.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+  };
 
-    setupOpen.addEventListener('click', function() {
-        openPopup();
-    });
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
 
-    var onPopupEscPress = function (evt) {
-        var target = evt.target;
-        if (target.className !== 'setup-user-name') {
-            window.util.isEscEvent(evt, closePopup);
-        }
-    };
+  var onPopupEscPress = function (evt) {
+    var target = evt.target;
+    if(target.className !== 'setup-user-name') {
+      window.util.isEscEvent(evt, closePopup);
+    }
+  };
 
-    setupOpen.addEventListener('keydown', function(evt) {
-        window.util.isEnterEvent(evt, openPopup);
-    });
+  setupOpen.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, openPopup);
+  });
 
-    setupClose.addEventListener('click', function() {
-        closePopup();
-    });
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
 
-    setupClose.addEventListener('keydown', function(evt) {
-        window.util.isEnterEvent(evt, closePopup);
-    });
+  setupClose.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, closePopup);
+  });
 
-/*отправка формы*/
+  /*отправка формы*/
   form.addEventListener('submit', function (evt) {
-      window.backend.save(new FormData(form), function (response) {
-        closePopup();
-      });
-      evt.preventDefault();
+    window.backend.save(new FormData(form), function (response) {
+      closePopup();
     });
+    evt.preventDefault();
+  });
 
-    /*возможность перетаскивания диалога*/
-    var dialogHandle = setup.querySelector('.setup-user-pic');
-    dialogHandle.style.zIndex = 1000;
-    var DialogMove = function (evt) {
-        evt.preventDefault();
+  /*возможность перетаскивания диалога*/
+  var dialogHandler = setup.querySelector('.upload');
+  /* dialogHandle.style.zIndex = 1000;*/
+  var DialogMove = function(evt) {
+    evt.preventDefault();
 
-        var startCoords = {
-            x: evt.clientX,
-            y: evt.clientY
-        };
-
-        var onMouseMove = function (moveEvt) {
-            moveEvt.preventDefault();
-
-            var shift = {
-                x: startCoords.x - moveEvt.clientX,
-                y: startCoords.y - moveEvt.clientY
-            };
-
-            startCoords = {
-                x: moveEvt.clientX,
-                y: moveEvt.clientY
-            };
-
-            setup.style.top = (setup.offsetTop - shift.y) + 'px';
-            setup.style.left = (setup.offsetLeft - shift.x) + 'px';
-        };
-
-        var onMouseUp = function (upEvt) {
-            upEvt.preventDefault();
-            document.removeEventListener('mousemove', onMouseMove);
-            document.removeEventListener('mouseup', onMouseUp);
-        };
-
-        document.addEventListener('mousemove', onMouseMove);
-        document.addEventListener('mouseup', onMouseUp);
-
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
     };
 
-    dialogHandle.addEventListener('mousedown', DialogMove);
 
-    /*перемещение предметов*/
-    var shopElement = document.querySelector('.setup-artifacts-shop');
-    var draggedItem = null;
+    var dragged = false;
 
-    shopElement.addEventListener('dragstart', function(evt) {
-        if (evt.target.tagName.toLowerCase() === 'img') {
-             artifactsElement.style.outline = '2px dashed red';
-             draggedItem = evt.target;
-            evt.dataTransfer.setData('text/plain', evt.target.alt);
-        }
-    });
-    var artifactsElement = document.querySelector('.setup-artifacts');
 
-    artifactsElement.addEventListener('dragover', function(evt) {
-        evt.preventDefault();
-        return false;
-    });
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
 
-    artifactsElement.addEventListener('drop', function(evt) {
-        evt.target.style.backgroundColor = '';
-        evt.target.appendChild(draggedItem);
-        evt.preventDefault();
-    });
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
 
-    artifactsElement.addEventListener('dragenter', function(evt) {
-        artifactsElement.style.outline = '';
-        evt.target.style.backgroundColor = 'yellow';
-        evt.preventDefault();
-    });
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
 
-    artifactsElement.addEventListener('dragleave', function(evt) {
-        evt.target.style.backgroundColor = '';
-        evt.preventDefault();
-    });
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+
+      if(dragged) {
+        var onClickPreventDefault = function (evt) {
+          evt.preventDefault();
+          dialogHandler.removeEventListener('click', onClickPreventDefault)
+        };
+        dialogHandler.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+  };
+
+  dialogHandler.addEventListener('mousedown', DialogMove);
+
+  /*перемещение предметов*/
+  var shopElement = document.querySelector('.setup-artifacts-shop');
+  var draggedItem = null;
+
+  shopElement.addEventListener('dragstart', function (evt) {
+    if(evt.target.tagName.toLowerCase() === 'img') {
+      artifactsElement.style.outline = '2px dashed red';
+      draggedItem = evt.target;
+      evt.dataTransfer.setData('text/plain', evt.target.alt);
+    }
+  });
+  var artifactsElement = document.querySelector('.setup-artifacts');
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    evt.preventDefault();
+    return false;
+  });
+
+  artifactsElement.addEventListener('drop', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.target.appendChild(draggedItem);
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    artifactsElement.style.outline = '';
+    evt.target.style.backgroundColor = 'yellow';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.preventDefault();
+  });
 
 })();
